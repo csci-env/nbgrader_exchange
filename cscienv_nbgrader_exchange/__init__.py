2b99250b4e77873b7ab711baf1195747702d9e3f
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2024-07-11 14:52:12 -0400

    Simplify project

--- cscienv_nbgrader_exchange/__init__.py
+++ cscienv_nbgrader_exchange/__init__.py
@@ -1,31 +1,12 @@
 from ._version import __version__
-from .handlers import setup_handlers
-
-
-
-def _jupyter_labextension_paths():
-    return [{
-        "src": "labextension",
-        "dest": "cscienv_nbgrader_exchange"
-    }]
-
-
+from jupyter_server.serverapp import ServerApp
 
 def _jupyter_server_extension_points():
     return [{
         "module": "cscienv_nbgrader_exchange"
     }]
 
-
-def _load_jupyter_server_extension(server_app):
-    """Registers the API handler to receive HTTP requests from the frontend extension.
-
-    Parameters
-    ----------
-    server_app: jupyterlab.labapp.LabApp
-        JupyterLab application instance
-    """
-    setup_handlers(server_app.web_app)
+def _load_jupyter_server_extension(server_app: ServerApp):
     name = "cscienv_nbgrader_exchange"
     server_app.log.info(f"Registered {name} server extension")
 
