9459704beda29b2dcfd16e48c927e85571b093e3
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2024-07-16 11:18:45 -0400

    Add custom submit/collect logic

--- cscienv_nbgrader_exchange/collect.py
+++ cscienv_nbgrader_exchange/collect.py
@@ -5,6 +5,15 @@ from nbgrader.utils import check_mode
 import os
 
 class ExchangeCollect(collect.ExchangeCollect):
+    def _path_to_record(self, path):
+        """
+        Wraps the default _path_to_record function adjusting the 'filename' key
+        to prepend the student specific folder.
+        """
+        record = super()._path_to_record(path)
+        record['filename'] = os.path.join(record['username'], record['filename'])
+        return record
+
     def init_src(self):
         if self.coursedir.course_id == '':
             self.fail("No course id specified. Re-run with --course flag.")
@@ -16,9 +25,9 @@ class ExchangeCollect(collect.ExchangeCollect):
         if not check_mode(self.inbound_path, read=True, execute=True):
             self.fail("You don't have read permissions for the directory: {}".format(self.inbound_path))
         student_id = self.coursedir.student_id if self.coursedir.student_id else '*'
-        pattern = os.path.join(self.inbound_path, '{}+{}+*'.format(student_id, self.coursedir.assignment_id))
+        pattern = os.path.join(self.inbound_path, '{}'.format(student_id), '{}+{}+*'.format(student_id, self.coursedir.assignment_id))
         records = [self._path_to_record(f) for f in glob.glob(pattern)]
-        usergroups = groupby(records, lambda item: item['username'])
+        usergroups = collect.groupby(records, lambda item: item['username'])
 
         with Gradebook(self.coursedir.db_url, self.coursedir.course_id) as gb:
             try:
--- cscienv_nbgrader_exchange/submit.py
+++ cscienv_nbgrader_exchange/submit.py
@@ -10,12 +10,6 @@ class ExchangeSubmit(submit.ExchangeSubmit):
         if not self.authenticator.has_access(self.coursedir.student_id, self.coursedir.course_id):
             self.fail("You do not have access to this course.")
 
-        self.inbound_path = os.path.join(self.root, self.coursedir.course_id, 'inbound')
-        if not os.path.isdir(self.inbound_path):
-            self.fail("Inbound directory doesn't exist: {}".format(self.inbound_path))
-        if not check_mode(self.inbound_path, write=True, execute=True):
-            self.fail("You don't have write permissions to the directory: {}".format(self.inbound_path))
-
         self.cache_path = os.path.join(self.cache, self.coursedir.course_id)
         if self.coursedir.student_id != '*':
             # An explicit student id has been specified on the command line; we use it as student_id
@@ -24,6 +18,13 @@ class ExchangeSubmit(submit.ExchangeSubmit):
             student_id = self.coursedir.student_id
         else:
             student_id = get_username()
+
+        self.inbound_path = os.path.join(self.root, self.coursedir.course_id, 'inbound', student_id)
+        if not os.path.isdir(self.inbound_path):
+            self.fail("Inbound directory doesn't exist: {}".format(self.inbound_path))
+        if not check_mode(self.inbound_path, write=True, execute=True):
+            self.fail("You don't have write permissions to the directory: {}".format(self.inbound_path))
+
         if self.add_random_string:
             random_str = base64.urlsafe_b64encode(os.urandom(9)).decode('ascii')
             self.assignment_filename = '{}+{}+{}+{}'.format(
@@ -31,4 +32,3 @@ class ExchangeSubmit(submit.ExchangeSubmit):
         else:
             self.assignment_filename = '{}+{}+{}'.format(
                 student_id, self.coursedir.assignment_id, self.timestamp)
-
